6ad68ae1010957700a80f5a3310e66125fc28a0b
pedrosortega <psortega2004@gmail.com>
2025-06-26 15:40:05 -0400

    get all tasks

--- api/tasks.js
+++ api/tasks.js
@@ -4,11 +4,23 @@ const { Task } = require("../dummy-database");
 
 // GET all tasks
 router.get("/", (req, res) => {
-  // Replace this with your code!
-  res.status(501).send("Not implemented");
+  const tasks = Task.findAll();
+  try {
+    res.send(tasks);
+  } catch {
+    res.status(501).send("Not implemented");
+  }
 });
 
 // GET a single task by id
+router.get("/", (req, res) => {
+  const getId = Task.findByPk(1);
+  try {
+    res.send(getId);
+  } catch {
+    res.status(501).send("Not Implemented");
+  }
+});
 
 // Patch a task by id
 
