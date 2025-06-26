07e68738b537a0dd77da384f7fd0b700b50fd8d0
pedrosortega <psortega2004@gmail.com>
2025-06-26 17:44:09 -0400

    create new task

--- api/tasks.js
+++ api/tasks.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { Task } = require("../dummy-database");
 
+// router.use(express.json());
+// router.use(morgan)
+
 // GET all tasks
 router.get("/", (req, res) => {
-
   const tasks = Task.findAll();
   try {
     res.send(tasks);
   } catch {
     res.status(501).send("Not implemented");
   }
-
 });
 
 // GET a single task by id
@@ -30,5 +31,16 @@ router.get("/:id", (req, res) => {
 // Delete a task by id
 
 // Create a new task
+
+router.post("/", (req, res) => {
+  const taskTitle = req.body.title;
+  const taskDesc = req.body.description;
+  const newTask = {
+    title: taskTitle,
+    description: taskDesc,
+  };
+  Task.create(newTask);
+  res.sendStatus(201);
+});
 
 module.exports = router;
